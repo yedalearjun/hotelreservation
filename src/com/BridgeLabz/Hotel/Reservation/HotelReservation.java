0659a3d0eaec34049a618561f205879b92871b5a
ARJUN YEDALE <arjunyedale8748@gmail.com>
2023-12-25 21:33:24 +0530

    added to master

---
+++ src/com/BridgeLabz/Hotel/Reservation/HotelReservation.java
@@ -0,0 +1,7 @@
+package com.BridgeLabz.Hotel.Reservation;
+
+public class HotelReservation {
+    public static void main(String[] args) {
+        System.out.println("welcome to Hotel Reservation system");
+    }
+}
